8c75a3eb0850bd350929838bc300aec55e8d35f1
Monika Sharma <monic.sh@gmail.com>
2017-11-25 06:36:05 -0800

    Add file

---
+++ Queue.cpp
@@ -0,0 +1,13 @@
+//
+//  Queue.cpp
+//  MembershipProtocol
+//
+//  Created by Monika Sharma on 11/25/17.
+//  Copyright © 2017 Monika Sharma. All rights reserved.
+//
+
+
+#include <stdio.h>
+#include "Queue.h"
+
+q_elt::q_elt(void *elt, int size): elt(elt), size(size) {}
