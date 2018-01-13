f15921dd0d7ce063bbf49ae6c350903695a50998
Monika Sharma <monic.sh@gmail.com>
2018-01-13 06:33:15 -0800

    makefile

---
+++ QuorumTracker.cpp
@@ -0,0 +1,10 @@
+
+//
+//  QuorumTraacker.cpp
+//  MembershipProtocol
+//
+//  Created by Monika Sharma on 1/13/18.
+//  Copyright © 2018 Monika Sharma. All rights reserved.
+//
+
+#include "QuorumTraacker.h"
---
+++ QuorumTracker.h
@@ -0,0 +1,22 @@
+//
+//  QuorumTraacker.hpp
+//  MembershipProtocol
+//
+//  Created by Monika Sharma on 1/13/18.
+//  Copyright © 2018 Monika Sharma. All rights reserved.
+//
+
+#ifndef QuorumTraacker_hpp
+#define QuorumTraacker_hpp
+
+#include "stdincludes.h"
+
+class QuorumTracker
+{
+
+public:
+    virtual ~QuorumTracker();
+    QuorumTracker();
+
+};
+#endif /* QuorumTraacker_hpp */
